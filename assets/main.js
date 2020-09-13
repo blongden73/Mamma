1e93b4c50293d4ebcc3b651c38ae9c2ad974c996
blongden73 <hello@benlongdendesign.co.uk>
2020-09-13 21:00:19 +0100

    people data

--- assets/main.js
+++ assets/main.js
@@ -6,3 +6,27 @@ $(".mega-title").html(function(){
   var last = text.pop();
   return text.join(" ") + (text.length > 0 ? " <span class='underline'>" + last + "</span>" : last);
 });
+
+//ajax call for people
+function loadJSON(path, success, error){
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function()
+  {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+          if (xhr.status === 200) {
+              if (success)
+                  success(JSON.parse(xhr.responseText));
+          } else {
+              if (error)
+                  error(xhr);
+          }
+      }
+  };
+  xhr.open("GET", path, true);
+  xhr.send();
+}
+loadJSON("https://mamma-join.neuer.uk/wp-json/mamma/v1/get-members/", function(data) {
+  console.log(data);
+}, function(xhr) {
+  console.log("error");
+});
