0bbb0d94384e8985303690c2cbcfce978e452e70
blongden73 <hello@benlongdendesign.co.uk>
2020-09-27 23:34:36 +0100

    file

--- assets/main.js
+++ assets/main.js
@@ -44,7 +44,10 @@ if(peopleWrapper || practitionerWrapper) {
     methods: {
       log: function() {
         console.log(this, 'vue data');
-      }
+      },
+      itemsContains(n) {
+      return this.items.indexOf(n) > -1
+    }
     }
   })
   app.log();
