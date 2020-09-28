0fef7e3fd7a5295945f4be9382194605178b4be8
blongden73 <hello@benlongdendesign.co.uk>
2020-09-28 01:32:29 +0100

    file

--- assets/main.js
+++ assets/main.js
@@ -174,12 +174,15 @@ function calFilters() {
   var filter = document.querySelector('.practices-filter');
   var options = filter.querySelectorAll('option');
   console.log(options);
-  for(i = 0; i < options.length; i++) {
-    options[i].addEventListener('click', function(){
-      var selector = options[i].value;
-      console.log(selector);
-    });
-  }
+  filter.addEventListener('change', function(){
+    console.log(this.value);
+  });
+  // for(i = 0; i < options.length; i++) {
+  //   options[i].addEventListener('click', function(){
+  //     var selector = options[i].value;
+  //     console.log(selector);
+  //   });
+  // }
 }
 
 calFilters();
