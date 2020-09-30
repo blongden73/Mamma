a5ac5c022205adb9de0ad684058f144149b1bcd2
blongden73 <hello@benlongdendesign.co.uk>
2020-09-30 23:02:02 +0100

    search

--- assets/main.js
+++ assets/main.js
@@ -214,7 +214,7 @@ function videoPlayers(){
       var iframe = this.nextSibling;
       placeholder[i].addEventListener('click', function(){
         this.classList.add('hide');
-        var frameWrapper = this.nextSibling.dataset.frame;
+        var frameWrapper = this.nextSibling;
         console.log(frameWrapper);
       });
     }
