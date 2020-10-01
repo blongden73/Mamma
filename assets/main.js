6acccafbd6bdb178f29af588d732a843fd353f83
blongden73 <hello@benlongdendesign.co.uk>
2020-10-01 22:53:57 +0100

    bugs

--- assets/main.js
+++ assets/main.js
@@ -233,7 +233,7 @@ function tagsearch(){
     var search = document.querySelector('.custom-m-search');
     console.log(offering.value, how.value, wellbeing.value, search.value);
     if(search.value) {
-      var url = '/search?q=' + search.value + '+tag%3A' + offering.value + ':' + wellbeing.value;
+      var url = '/search?q=' + search.value + '+tag%3A' + offering.value.replace(' ', '+') + '%3A' + wellbeing.value;
       console.log(url);
       window.location.replace(url);
     }
