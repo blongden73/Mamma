731e80dc88302a3e6a09e8dc33801d3532cb7ec5
blongden73 <hello@benlongdendesign.co.uk>
2020-10-14 14:40:30 +0100

    search bug

--- assets/main.js
+++ assets/main.js
@@ -241,13 +241,13 @@ function tagsearch(){
     var wellbeing = document.querySelector('.wellbeing-select');
     var search = document.querySelector('.custom-m-search');
     console.log(offering.value, how.value, wellbeing.value, search.value);
-    if(url && !how && !wellbeing && !offering) {
+    if(search && !how && !wellbeing && !offering) {
       var url = '/search?q=' + search.value;
     }
-    if(url && !how && !wellbeing && offering) {
+    if(search && !how && !wellbeing && offering) {
       var url = '/search?q=' + search.value + '+tag%3A' + offering.value.replace(' ', '+');
     }
-    if(url && !how && wellbeing && offering) {
+    if(search && !how && wellbeing && offering) {
       var url = '/search?q=' + search.value + '+tag%3A' + offering.value.replace(' ', '+') + '+tag%3A' + wellbeing.value;
     }
 
