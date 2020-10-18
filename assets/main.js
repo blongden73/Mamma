3949163e80671654233065b45c47b84ebce7d196
blongden73 <hello@benlongdendesign.co.uk>
2020-10-18 21:20:55 +0100

    product template

--- assets/main.js
+++ assets/main.js
@@ -47,7 +47,7 @@ if(peopleWrapper || practitionerWrapper) {
       }
     }
   })
-  Vue.filter('cleaner', function (value) {
+  Vue.filter('cleaning', function (value) {
     console.log('running function')
     var block = value.split('\r\n');
     console.log(block.length, 'split');
