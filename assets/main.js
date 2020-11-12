5bb6931bedd8bcdf61cb6c0a2ae26459e2541b58
blongden73 <hello@benlongdendesign.co.uk>
2020-11-12 22:45:16 +0000

    added testing link

--- assets/main.js
+++ assets/main.js
@@ -37,7 +37,7 @@ if(peopleWrapper || practitionerWrapper) {
         practioners: []
     },
     mounted: function() {
-      $.get('https://cdn.shopify.com/s/files/1/0459/1697/9349/t/1/assets/Mamma.json?v=1034453513220108260', function(data) {
+      $.get('https://testing.joinmamma.com/wp-json/mamma/v1/get-members/', function(data) {
           app.practioners = data;
       })
     },
